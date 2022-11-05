5c782adf553131ef72efea87c3ba7f54f1ad6d47
xander04301 <110802453+xander04301@users.noreply.github.com>
2022-11-05 21:07:58 +0800

    update
    Added Currency

--- project3.py
+++ project3.py
@@ -42,6 +42,7 @@ def get_ip_location():
         "region": response.get("region"),
         "country": response.get("country_name"),
         "country code": response.get("country_code"),
+        "currency": response.get("currency_name"),
         "latitude": response.get("latitude"),
         "longitude": response.get("longitude"),
         "asn": response.get("asn"),
